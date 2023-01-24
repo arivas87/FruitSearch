2855b5124eebc2c77723567cef90247c67bcccae
Arturo Rivas <arturo.rivas@icloud.com>
2023-01-24 11:19:54 +0100

    Initial Commit

---
+++ FruitSearch/FruitSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitSearchApp.swift
+//  FruitSearch
+//
+//  Created by Arturo Rivas Arias on 24/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
